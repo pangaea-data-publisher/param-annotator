24b23bb4284d468ab7449f2e1e40284e144f9dc9
Uwe Schindler <uwe@thetaphi.de>
2020-09-22 15:54:36 +0200

    fix startup parameter

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 cd `dirname $0`
 exec /usr/bin/daemon -o daemon.info -r -n param-annotator -i -D "$PWD" -- \
- python3 api/appv1.py config/termconf_production.ini
+ python3 api/appv1.py -c config/termconf_production.ini
